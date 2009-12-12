5052d7baddcf4d2b65228df8d65c7851b1f67e3d
delano <delano@solutious.com>
2009-12-12 02:50:43 -0500

    Basics

--- lib/bone.rb
+++ lib/bone.rb
@@ -1,5 +1,32 @@
 
 module Bone
+  extend self
+  
+  SOURCE = ENV['BONE_SOURCE'] || "localhost"
+  CID = 'a462b9ebda71f16cb1567ba8704695ae8dba9999'
+  
+  def get(cid, key)
+    resp = request :get, cid, key
+  end
+  
+  def set(cid, key, value)
+    value = File.readlines(value).join if File.exists?(value)
+    resp = request :set, cid, key, :value => URI.encode(value)
+    value
+  end
+  
+  def request(a, cid, key, params={})
+    uri_str = "http://#{SOURCE}:3000/#{a}/#{key}?cid=#{cid}"
+    params.each_pair {|n,v| uri_str << "&#{n}=#{v}" }
+    #p uri_str
+    uri = URI.parse uri_str
+    Net::HTTP.get(uri)
+  end
+  
+  module Aware
+    def bone(key)
+    end
+  end
   
 end
 
--- lib/bone/cli.rb
+++ lib/bone/cli.rb
@@ -1,5 +1,17 @@
 require 'bone'
+require 'net/http'
 
-class Bone::CLI
+class Bone::CLI < Drydock::Command
+  
+  def get
+    @argv.unshift @alias unless @alias == 'get'
+    #puts "KEYS: " << @argv.inspect
+    puts Bone.get(Bone::CID, @argv.first)
+  end
+  
+  def set
+    #puts "KEYS: " << @argv.inspect
+    puts Bone.set(Bone::CID, @argv[0], @argv[1])
+  end
   
 end
