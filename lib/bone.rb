c82fe8d0b9a31c7bf0b3811d4dd6690d2c6bffd2
delano <delano@solutious.com>
2009-12-12 12:35:10 -0500

    bin/bone is alive

--- lib/bone.rb
+++ lib/bone.rb
@@ -2,25 +2,45 @@
 module Bone
   extend self
   
-  SOURCE = ENV['BONE_SOURCE'] || "localhost"
-  CID = 'a462b9ebda71f16cb1567ba8704695ae8dba9999'
+  @debug = false
+  class << self
+    def enable_debug()  @debug = true  end
+    def disable_debug() @debug = false end
+    def debug?()        @debug == true end
+    def ld(*msg)
+      return unless Bone.debug?
+      prefix = "D(#{Thread.current.object_id}):  "
+      STDERR.puts "#{prefix}" << msg.join("#{$/}#{prefix}")
+    end
+  end
   
-  def get(cid, key)
+  SOURCE = (ENV['BONE_SOURCE'] || "localhost:6043").freeze
+  CID = ENV['BONE_CID'].freeze
+  
+  def get(key, opts={})
+    cid = opts[:cid] || CID
     resp = request :get, cid, key
   end
   
-  def set(cid, key, value)
+  def set(key, value, opts={})
+    cid = opts[:cid] || CID
     value = File.readlines(value).join if File.exists?(value)
     resp = request :set, cid, key, :value => URI.encode(value)
     value
   end
   
-  def request(a, cid, key, params={})
-    uri_str = "http://#{SOURCE}:3000/#{a}/#{key}?cid=#{cid}"
-    params.each_pair {|n,v| uri_str << "&#{n}=#{v}" }
-    #p uri_str
-    uri = URI.parse uri_str
+  def request(action, cid, key, params={})
+    params[:cid] = cid
+    uri = "http://#{SOURCE}/#{action}/#{key}?"
+    args = []
+    params.each_pair {|n,v| args << "#{n}=#{URI.encode(v)}" }
+    uri = URI.parse(uri << args.join('&'))
+    Bone.ld "URI: #{uri}"
     Net::HTTP.get(uri)
+  rescue SocketError => ex
+    STDERR.puts "No boned"
+    STDERR.puts ex.message, ex.backtrace if Drydock.debug?
+    exit 1
   end
   
   module Aware
--- lib/bone/cli.rb
+++ lib/bone/cli.rb
@@ -6,12 +6,12 @@ class Bone::CLI < Drydock::Command
   def get
     @argv.unshift @alias unless @alias == 'get'
     #puts "KEYS: " << @argv.inspect
-    puts Bone.get(Bone::CID, @argv.first)
+    puts Bone.get(@argv.first)
   end
   
   def set
     #puts "KEYS: " << @argv.inspect
-    puts Bone.set(Bone::CID, @argv[0], @argv[1])
+    puts Bone.set(@argv[0], @argv[1])
   end
   
 end
